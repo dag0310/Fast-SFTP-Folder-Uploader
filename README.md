302873b460b412d3a7c7dd4dbfc2effc8572d1ba
Daniel Geymayer <dag0310@gmail.com>
2023-09-13 16:26:28 +0200

    Add ability to select zip file alternatively to folder

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Fast SFTP Folder Uploader
 
-Uploads a folder fast to a set SSH remote folder location by zipping it on the client, uploading the zip file and unzipping it on the remote host.
+Uploads a folder fast to a set SFTP remote folder location by zipping it on the client, uploading the zip file and unzipping it on the remote host.
 
 It also automatically deletes the temporary zip files locally and remotely.
 
 Very useful for folders with many files and folders, like a Wordpress instance.
 
-If you just want to upload a zip file use any other tool like FileZilla.
+You can also just upload an existing ZIP file, which will not be deleted locally.
+
+If you just want to upload a zip file without remote extraction use any other tool like FileZilla.
 
 ## Requirements
 
--- fast_sftp_folder_uploader.py
+++ fast_sftp_folder_uploader.py
@@ -2,12 +2,11 @@ import tkinter as tk
 from tkinter import StringVar, filedialog
 import paramiko
 import os
-import platform
 import configparser
 
-# Create or read the configuration file
 config = configparser.ConfigParser()
 config_file = 'config.ini'
+zip_file_extension = '.zip'
 
 if os.path.exists(config_file):
     config.read(config_file)
@@ -18,86 +17,106 @@ def update_status(text):
     output_text.set(text)
 
 
-def zip_and_upload(local_folderpath, remote_folderpath, hostname, username, password):
-    zip_filename = None
+def zip_and_upload(local_path, remote_folderpath, hostname, username, password):
+    zip_filepath = None
+    is_temporary_zip_file = False
     try:
-        if local_folderpath == '':
-            raise Exception("Please select a folder.")
+        if local_path == '':
+            raise Exception("Please select a folder or ZIP file.")
 
-        # Create a temporary zip file
-        update_status('[LOCAL] Preparing folder for upload ...')
-        cwd = os.getcwd()
-        folder_name = os.path.basename(local_folderpath)
-        zip_filename = folder_name + ".zip"
-        os.chdir(local_folderpath)
-        os.chdir('..')
-        zip_command = 'tar -a -c -f "' + os.path.join(cwd, zip_filename) + '" "' + folder_name + '"'
-        print('[LOCAL] ' + zip_command)
-        os.system(zip_command)
-        os.chdir(cwd)
+        basename = os.path.basename(local_path)
+        if basename.endswith(zip_file_extension):
+            print('Using selected ZIP file.')
+            is_temporary_zip_file = False
+            zip_filename = basename
+            zip_filepath = local_path
+        else:
+            print('Creating temporary ZIP file ...')
+            is_temporary_zip_file = True
+            cwd = os.getcwd()
+            folder_name = os.path.basename(local_path)
+            zip_filename = folder_name + zip_file_extension
+            zip_filepath = os.path.join(cwd, zip_filename)
+            os.chdir(local_path)
+            os.chdir('..')
+            zip_command = 'tar -a -c -f "' + zip_filepath + '" "' + folder_name + '"'
+            print(zip_command)
+            os.system(zip_command)
+            os.chdir(cwd)
 
-        # Create an SSH client
-        update_status('[LOCAL] Establishing connection to remote ...')
+        print('Creating SSH client ...')
         ssh_client = paramiko.SSHClient()
         ssh_client.load_system_host_keys()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        # Connect to the remote host
+        print('Connecting to the remote host ...')
         ssh_client.connect(hostname, username=username, password=password)
 
-        # Create an SFTP client
+        print('Creating SFTP client ...')
         sftp = ssh_client.open_sftp()
 
-        # Upload the zip file to the remote folder
-        update_status('[LOCAL] Uploading to remote ...')
-        if remote_folderpath == '':
-            remote_filepath = zip_filename
-        elif remote_folderpath[-1] == '/':
+        print('Uploading ZIP file to remote folder ...')
+        if remote_folderpath == '' or remote_folderpath[-1] == "/":
             remote_filepath = remote_folderpath + zip_filename
         else:
             remote_filepath = remote_folderpath + "/" + zip_filename
 
-        print("[LOCAL] sftp.put('" + zip_filename + "', '" + remote_filepath + "')")
-        sftp.put(zip_filename, remote_filepath)
+        print("sftp.put('" + zip_filepath + "', '" + remote_filepath + "')")
+        sftp.put(zip_filepath, remote_filepath)
 
-        # Execute the unzip command remotely
-        update_status('[REMOTE] Saving folder ...')
-        remote_unzip_command = 'unzip ' + remote_filepath + ' -d ' + remote_folderpath
-        ssh_client.exec_command(remote_unzip_command)
-        print('[REMOTE] ' + remote_unzip_command)
-        remote_rm_command = 'rm ' + remote_filepath
-        ssh_client.exec_command(remote_rm_command)
-        print('[REMOTE] ' + remote_rm_command)
-
-        # Close the SFTP session and SSH connection
+        print('Closing SFTP connection ...')
         sftp.close()
+
+        print('Unzipping the file remotely ...')
+        remote_unzip_command = 'unzip -qq "' + remote_filepath + '" -d "' + remote_folderpath + '"'  # -qq necessary for reliable complete unzipping!
+        print(remote_unzip_command)
+        ssh_client.exec_command(remote_unzip_command)
+
+        print('Deleting the temporary ZIP file remotely ...')
+        remote_rm_command = 'rm "' + remote_filepath + '"'
+        print(remote_rm_command)
+        ssh_client.exec_command(remote_rm_command)
+
+        print('Closing SSH connection ...')
         ssh_client.close()
 
-        update_status('[REMOTE] Folder uploaded successfully!')
+        update_status('Upload successful!')
     except Exception as e:
         update_status(str(e))
     finally:
-        # Remove the temporary zip file
-        if zip_filename is not None and os.path.exists(zip_filename):
-            os.remove(zip_filename)
-            print("[LOCAL] Temporary local zip file removed.")
+        if is_temporary_zip_file and zip_filepath is not None and os.path.exists(zip_filepath):
+            os.remove(zip_filepath)
+            print('Temporary local zip file removed.')
 
 
 def browse_folder():
-    local_folder = filedialog.askdirectory()
-    local_folder_entry.delete(0, tk.END)
-    local_folder_entry.insert(0, local_folder)
+    local_path = filedialog.askdirectory()
+    if local_path == '':
+        return
+    local_path_entry.delete(0, tk.END)
+    local_path_entry.insert(0, local_path)
+    upload_button.config(text='Upload folder')
     update_status('Folder selected.')
 
 
+def browse_zip_file():
+    local_path = filedialog.askopenfilename(filetypes=[('ZIP files', '*' + zip_file_extension)])
+    if local_path == '':
+        return
+    local_path_entry.delete(0, tk.END)
+    local_path_entry.insert(0, local_path)
+    upload_button.config(text='Upload ZIP file')
+    update_status('ZIP file selected.')
+
+
 def upload():
-    local_folder = local_folder_entry.get()
+    local_path = local_path_entry.get()
     remote_folder = remote_folder_entry.get()
     hostname = hostname_entry.get()
     username = username_entry.get()
     password = password_entry.get()
 
-    zip_and_upload(local_folder, remote_folder, hostname, username, password)
+    zip_and_upload(local_path, remote_folder, hostname, username, password)
 
 
 def save_config():
@@ -108,41 +127,52 @@ def save_config():
 
     with open(config_file, 'w') as configfile:
         config.write(configfile)
+
     update_status('Config saved.')
 
 
 # Create the main window
+padding = 15
 root = tk.Tk()
 root.title("Fast SFTP Folder Uploader")
+root.geometry('400x400')
 
 # Local folder selection
-local_folder_label = tk.Label(root, text="Local Folder:")
-local_folder_label.pack()
-local_folder_entry = tk.Entry(root)
-local_folder_entry.pack()
-browse_button = tk.Button(root, text="Browse", command=browse_folder)
-browse_button.pack()
+local_path_label = tk.Label(root, text="Local folder or ZIP file:")
+local_path_label.pack()
+local_path_entry = tk.Entry(root)
+local_path_entry.pack(fill="x", padx=padding)
+
+browse_buttons_frame = tk.Frame(root)
+browse_buttons_frame.pack(pady=20)
+
+browse_folder_button = tk.Button(browse_buttons_frame, text="Select folder", command=browse_folder)
+browse_folder_button.pack(side="left")
+local_path_label = tk.Label(browse_buttons_frame, text="or")
+local_path_label.pack(side="left", padx=5, pady=5)
+browse_zip_file_button = tk.Button(browse_buttons_frame, text="Select ZIP file", command=browse_zip_file)
+browse_zip_file_button.pack(side="left")
 
 # Remote folder and SFTP settings
 remote_folder_label = tk.Label(root, text="Remote Folder:")
 remote_folder_label.pack()
 remote_folder_entry = tk.Entry(root)
-remote_folder_entry.pack()
+remote_folder_entry.pack(fill="x", padx=padding)
 
-hostname_label = tk.Label(root, text="SFTP Hostname:")
+hostname_label = tk.Label(root, text="Hostname:")
 hostname_label.pack()
 hostname_entry = tk.Entry(root)
-hostname_entry.pack()
+hostname_entry.pack(fill="x", padx=padding)
 
 username_label = tk.Label(root, text="Username:")
 username_label.pack()
 username_entry = tk.Entry(root)
-username_entry.pack()
+username_entry.pack(fill="x", padx=padding)
 
 password_label = tk.Label(root, text="Password:")
 password_label.pack()
 password_entry = tk.Entry(root, show="*")
-password_entry.pack()
+password_entry.pack(fill="x", padx=padding)
 
 # Load saved configuration
 remote_folder_entry.insert(0, config['SFTP']['remote_folder'])
@@ -150,16 +180,17 @@ hostname_entry.insert(0, config['SFTP']['hostname'])
 username_entry.insert(0, config['SFTP']['username'])
 password_entry.insert(0, config['SFTP']['password'])
 
-# Save button
+# Save config button
 save_button = tk.Button(root, text="Save Config", command=save_config)
 save_button.pack()
 
 # Upload button
-upload_button = tk.Button(root, text="Upload folder", command=upload)
+upload_button = tk.Button(root, text="Upload", command=upload)
 upload_button.pack()
 
+# Status output label
 output_text = StringVar()
-output_text.set('Select a local folder to upload.')
+output_text.set('Select a local folder or ZIP file to upload.')
 output_label = tk.Label(root, textvariable=output_text)
 output_label.pack()
 
